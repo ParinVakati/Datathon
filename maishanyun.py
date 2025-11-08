83343bc2b46bf5950cbe69d8c07ce4a7c742ad3b
ParinVakati <127551816+ParinVakati@users.noreply.github.com>
2025-11-08 13:49:35 -0600

    Co-authored-by: Kaushal Prasath <kaushalprasath@gmail.com> Co-authored-by: Siddharth <aguw0kie124@users.noreply.github.com>

--- maishanyun.py
+++ maishanyun.py
@@ -2,3 +2,15 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 
+def load_monthly_sales(file_paths):
+    #loads all monthly sales xlsx files and combines them
+    #pile_paths is the dictionaly with month as key and file path as value
+
+    sales_data = {}
+
+    for month, filepath in file_paths.items():
+        df = pd.read_excel(filepath)
+        df['month'] = month
+        sales_data[month] = df
+
+    file = open("Ingredient.csv", "r")
